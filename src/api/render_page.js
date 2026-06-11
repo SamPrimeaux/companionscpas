a5e0c34b937347adfc26dd99f06b23eb933407d0
Sam Primeaux <ceosamprimeaux@gmail.com>
2026-06-11 02:01:44 -0500

    fix: bust cpas-shell.css browser cache with version query string

--- src/api/render_page.js
+++ src/api/render_page.js
@@ -229,7 +229,7 @@ export function assembleFullPage(page, brand, headerHtml, sectionHtmls, footerHt
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>${title}</title>
   <meta name="description" content="${description}">
-  <link rel="stylesheet" href="/static/global/cpas-shell.css">
+  <link rel="stylesheet" href="/static/global/cpas-shell.css?v=2026061102">
 ${fontImportTag}
 ${brandScript}
 </head>
